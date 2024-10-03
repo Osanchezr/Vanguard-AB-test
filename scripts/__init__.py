2fbe579b768d819b1e76a52a05af9091ba93d07e
Carlos Vergara <the.boss.patriot@gmail.com>
2024-10-03 18:32:07 +0200

    Update __init__.py
    Comment added

---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+# Este archivo __init__.py indica que este directorio debe ser tratado como un paquete de Python.
+# Su propósito es permitir la importación de módulos y funciones desde este paquete.
+# Asegúrate de no eliminar este archivo, ya que es esencial para la estructura del proyecto y
+# su funcionamiento correcto.
